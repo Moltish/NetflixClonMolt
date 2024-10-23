0bce146708fb703a84d0fb83f42c19d74c546497
Moltish <moltotalm@gmail.com>
2024-10-22 20:22:27 -0400

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/NetflixMerkato/",
+  base: "/NetflixClonMolt/",
   plugins: [react()],
 });
